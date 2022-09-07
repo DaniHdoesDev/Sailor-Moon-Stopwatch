c480ac734d023e10f6a3cb8c9e48a47e2039d340
Dani Hoskins <hoskins.dannala@gmail.com>
2022-09-06 19:16:33 -0500

    Mod to JS and css

--- Watch.js
+++ Watch.js
@@ -1,60 +1,81 @@
 window.onload = function () {
   
-    let seconds = 00; 
-    let tens = 00; 
-    let appendTens = document.getElementById("tens")
-    let appendSeconds = document.getElementById("seconds")
-    let buttonStart = document.getElementById('button-start');
-    let buttonStop = document.getElementById('button-stop');
-    let buttonReset = document.getElementById('button-reset');
-    let Interval ;
+    let startBtn = document.getElementById('start');
+let stopBtn = document.getElementById('stop');
+let resetBtn = document.getElementById('reset');
   
-    buttonStart.onclick = function() {
-      
-      clearInterval(Interval);
-       Interval = setInterval(startTimer, 10);
-    }
-    
-      buttonStop.onclick = function() {
-         clearInterval(Interval);
-    }
-    
+let hour = 00;
+let minute = 00;
+let second = 00;
+let count = 00;
   
-    buttonReset.onclick = function() {
-       clearInterval(Interval);
-      tens = "00";
-        seconds = "00";
-      appendTens.innerHTML = tens;
-        appendSeconds.innerHTML = seconds;
-    }
-    
-     
-    
-    function startTimer () {
-      tens++; 
-      
-      if(tens <= 9){
-        appendTens.innerHTML = "0" + tens;
-      }
-      
-      if (tens > 9){
-        appendTens.innerHTML = tens;
-        
-      } 
-      
-      if (tens > 99) {
-        console.log("seconds");
-        seconds++;
-        appendSeconds.innerHTML = "0" + seconds;
-        tens = 0;
-        appendTens.innerHTML = "0" + 0;
-      }
-      
-      if (seconds > 9){
-        appendSeconds.innerHTML = seconds;
-      }
-    
-    }
-    
+startBtn.addEventListener('click', function () {
+    timer = true;
+    stopWatch();
+});
   
-  }
+stopBtn.addEventListener('click', function () {
+    timer = false;
+});
+  
+resetBtn.addEventListener('click', function () {
+    timer = false;
+    hour = 0;
+    minute = 0;
+    second = 0;
+    count = 0;
+    document.getElementById('hr').innerHTML = "00";
+    document.getElementById('min').innerHTML = "00";
+    document.getElementById('sec').innerHTML = "00";
+    document.getElementById('count').innerHTML = "00";
+});
+
+
+function stopWatch() {
+    if (timer) {
+        count++;
+  
+        if (count == 100) {
+            second++;
+            count = 0;
+        }
+  
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+  
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+            second = 0;
+        }
+  
+        let hrString = hour;
+        let minString = minute;
+        let secString = second;
+        let countString = count;
+  
+        if (hour < 10) {
+            hrString = "0" + hrString;
+        }
+  
+        if (minute < 10) {
+            minString = "0" + minString;
+        }
+  
+        if (second < 10) {
+            secString = "0" + secString;
+        }
+  
+        if (count < 10) {
+            countString = "0" + countString;
+        }
+  
+        document.getElementById('hr').innerHTML = hrString;
+        document.getElementById('min').innerHTML = minString;
+        document.getElementById('sec').innerHTML = secString;
+        document.getElementById('count').innerHTML = countString;
+        setTimeout(stopWatch, 10);
+    }
+}
